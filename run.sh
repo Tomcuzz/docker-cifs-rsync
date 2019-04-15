a3338a1bccce724ddd71d456e1b7c545f88e8ede
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2019-04-15 02:02:40 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-echo "//$SHARE-SERVER/$SHARE-NAME    /mnt/remote    cifs    uid=0,gid=0,user=$SHARE-USER,password=$SHARE-PASS,_netdev 0 0" >> /etc/fstab
+echo "//$SHARE_SERVER/$SHARE_NAME    /mnt/remote    cifs    uid=0,gid=0,user=$SHARE_USER,password=$SHARE_PASS,_netdev 0 0" >> /etc/fstab
 mount -a; sleep 365d
