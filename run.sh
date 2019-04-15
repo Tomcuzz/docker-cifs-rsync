cb930472bce9af344191ef2cdd6606b00422281d
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2019-04-16 00:41:49 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+echo "Creating mount point"
 echo "//$SHARE_SERVER/$SHARE_NAME    /mnt/remote    cifs    uid=0,gid=0,user=$SHARE_USER,password=$SHARE_PASS,_netdev 0 0" >> /etc/fstab
+echo "Mounting Server"
 mount -a
-sleep 365d
+
+echo "Creating rsync cron job"
+echo "00 03     * * *   root    rsync -ahv --ignore-existing /local/ /mnt/remote/$SHARE_SYNCPATH" >> /etc/crontab
+
+tail -f /var/log/cron.log
